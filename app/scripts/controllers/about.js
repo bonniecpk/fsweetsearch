96131386c3d4a0cc6427c4d6944a7b3af5fc1e31
Po Ki Chui <pchui@flatworldknowledge.com>
2015-05-28 02:13:26 +0900

    Created Angular app scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fsweetsearchApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fsweetsearchApp
+ */
+angular.module('fsweetsearchApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fsweetsearchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fsweetsearchApp
+ */
+angular.module('fsweetsearchApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
